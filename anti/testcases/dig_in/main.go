44b1114327d7018a9936b184b68426c24e48bced
Vladimir Fetisov <vladimir.fetisov@pm.me>
2023-07-03 20:41:25 +0200

    Fix a dig_in bug (#6)

--- anti/testcases/dig_in/main.go
+++ anti/testcases/dig_in/main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	dig "github.com/3timeslazy/anti-dig"
 	"github.com/3timeslazy/anti-dig/anti/testcases/dig_in/server"
 )
 
 func main() {
-	// TODO: A bug case. Once the bug is fixed please remove
-	file, err := os.ReadFile("./testcases/dig_in/expected.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	os.Stdout.Write(file)
-	return
-
 	dig.Anti.Optimise(false)
 
 	container := dig.New()
